7cb90648649dc1eae73cbeb6099fe5ab839f2e87
ZAKA <154753212+zakasalman@users.noreply.github.com>
2026-07-24 21:29:36 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0 , right = s.length() - 1 ;
+        while(left < right){
+          if(!isalnum(s[left])) { left++; continue; }
+          if(!isalnum(s[right])) { right--; continue; }
+          if(tolower(s[left]) != tolower(s[right])) return false ;
+          left++; right--;
+        }
+        return true ;
+    }
+};
